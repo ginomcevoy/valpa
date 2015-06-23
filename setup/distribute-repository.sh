62faba1461a5f99a0aa32144136fd5fcca67552c
Giacomo Mc Evoy <gino.mcevoy@gmail.com>
2015-06-23 15:41:15 -0300

    Added distribute-repository.sh,Synchronizes the VM repositories of all nodes to the master node

---
+++ setup/distribute-repository.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Synchronizes the VM repositories of all nodes to the master node
+# Uses rsync (ansible synchronize) to upload disk images
+# Author: Giacomo Mc Evoy
+# LNCC Brazil 2015
+
+# calculate directory local to script
+LOCAL_DIR="$( cd "$( dirname "$0" )" && pwd )"
+VALPA_DIR=$LOCAL_DIR/..
+
+# Import params
+source $VALPA_DIR/params.sh
+
+# Generate inventory for whole cluster
+INVENTORY=$($VALPA_DIR/util/nodes-inventory.sh $NODE_L)
+
+# Create repository in nodes if it does not exist
+ansible all -f $NODE_L -i $INVENTORY -m command -a "mkdir -p $REPO_ROOT/$REPO_BASE"
+
+# Update VM images using rsync, one node at a time
+ansible all -f 1 -i $INVENTORY -m synchronize -a "src=$REPO_ROOT/$REPO_BASE/ dest=$REPO_ROOT/$REPO_BASE/"
