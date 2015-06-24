934393f8c7427864916a88367b6389193590ae3b
Giacomo Mc Evoy <gino.mcevoy@gmail.com>
2015-06-23 23:28:39 -0300

    Added inventory for VMs, includes VM parameters, closes #12

---
+++ test/integration/bean_vm.py
@@ -0,0 +1,29 @@
+'''
+Created on Jun 23, 2015
+
+@author: giacomo
+'''
+import unittest
+
+from test.test_abstract import ValpaWithNodesAbstractTest
+
+class TestVirtualClusterTemplates(ValpaWithNodesAbstractTest):
+    '''
+    Integration test for NodeCluster that uses the bootstrapper 
+    to create an Ansible inventory for the physical nodes.
+    '''
+    def setUp(self):
+        super(TestVirtualClusterTemplates, self).setUp()
+        self.inventoryFilename = '/tmp/valpa-vm-inventory.test'
+    
+    def testCreateInventory(self):
+        # when
+        self.allVMDetails.createInventory(self.inventoryFilename, self.physicalCluster)
+        
+        # then get expected content in the inventory file
+        actualContent = open(self.inventoryFilename, 'r').read()
+        expectedContent = open('resources/inventory-vm-expected.txt', 'r').read()
+        self.assertMultiLineEqual(actualContent, expectedContent, 'VM inventory')
+
+if __name__ == "__main__":
+    unittest.main()
